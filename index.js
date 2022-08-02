cbb1615b4445d96e82a21876f613271ad9064dfb
Your name <wong.shiuy@gmail.com>
2022-08-01 22:50:49 -0400

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 2 
+let num2 = 31
+const multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) +1;
+let num3 = 24
+let num4 = 5
+let mod = Math.floor(num3/num4)
+let max = Math.max(1, 2, 4, 5, 10, 20);
 // Write your code here
